1490dfdff54c9fd6865d327211c2193cbe5241d5
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-01-22 22:26:50 +0700

    Init commit

---
+++ test/aws-aurora-pgvector-extension-creator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsAuroraPgvectorExtensionCreator from '../lib/aws-aurora-pgvector-extension-creator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-aurora-pgvector-extension-creator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsAuroraPgvectorExtensionCreator.AwsAuroraPgvectorExtensionCreatorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
